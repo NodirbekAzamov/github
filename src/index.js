a7fe67e9d9a3af35cbe60b1e931a15c267af4b77
Nodirbek Azamov <u900.nodir@gmail.com>
2021-11-29 15:24:46 +0300

    master

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+
+    const headingStyle = {
+        color: 'red',
+        fontSize: 40,
+    }
+    return <>
+     <h1>uirevguevg</h1>
+    </>
+}
+ReactDOM.render(<App />, document.querySelector('#root'))
