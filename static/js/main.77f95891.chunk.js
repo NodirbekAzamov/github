adfe38882a6f8c7ca36f4fa051988a6d1dbb2e77
Nodirbek Azamov <u900.nodir@gmail.com>
2021-12-01 15:15:51 +0300

    Updates

---
+++ static/js/main.77f95891.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgithub=this.webpackJsonpgithub||[]).push([[0],{4:function(e,t,c){"use strict";c.r(t);c(1);var r=c(3),i=c.n(r),n=c(0),s=function(){return Object(n.jsxs)(n.Fragment,{children:[Object(n.jsx)("h1",{children:"helo word"}),Object(n.jsx)("div",{className:"mima",children:Object(n.jsx)("h1",{children:"vjtevbjetbvijjbi"})})]})};i.a.render(Object(n.jsx)(s,{}),document.querySelector("#root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.77f95891.chunk.js.map
