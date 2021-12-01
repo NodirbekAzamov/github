0569d8ae30198d6cd208fc7ac2d7255b765fea5f
Nodirbek Azamov <u900.nodir@gmail.com>
2021-12-01 15:05:37 +0300

    master

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-
-    const headingStyle = {
-        color: 'red',
-        fontSize: 40,
-    }
     return <>
-     <h1>uirevumyumuymyuevg</h1>
+    <h1>helo word</h1>
     </>
 }
-ReactDOM.render(<App />, document.querySelector('#root'))
+ 
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
