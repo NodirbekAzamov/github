703fddcf530b47a7a18f19420d6bc3fec3467933
Nodirbek Azamov <u900.nodir@gmail.com>
2021-12-01 15:22:44 +0300

    master

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 const App = () => {
     return <>
     <h1>helo word</h1>
     <div className="mima">
         <h1>vjtevbjetbvijjbi</h1>
     </div>
+    
     </>
 }
  
